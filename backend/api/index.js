a754ca07ab197aef202569b6d8e18159c807028e
MunSeungJun <sbs2469@naver.com>
2025-03-26 12:12:39 +0900

    cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,13 +1,7 @@
 import { MongoClient } from "mongodb";
 
-
-
-
-
-
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용 (*)
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
   const uri =
     "mongodb+srv://vercel-admin-user:8FPLjbXwx9znESJY@cluster0.jzdiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
