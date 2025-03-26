60c3ed3fd67c5bead59dab0e741dea0bddf1c5e6
MunSeungJun <sbs2469@naver.com>
2025-03-26 10:42:32 +0900

    backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,19 +12,25 @@ export default async function handler(req, res) {
     if (req.method === "GET") {
       const cursor = coll.find({ employee_id: req.params.id });
       const users = await cursor.toArray();
-      return users;
+      res.status(200).json(users);
     }
-
     if (req.method === "POST") {
       coll.insertOne(req.body);
+      res.status(200).json({
+        message: "success",
+      });
     }
-
     if (req.method === "PUT") {
         coll.updateOne({employee_id: req.params.id}, req.body)
+        res.status(200).json({
+            message: "success",
+          });
     }
-
     if (req.method === "DELETE") {
         coll.deleteOne({ employee_id: req.params.id })
+        res.status(200).json({
+            message: "success",
+          });
     }
   } catch (e) {
     console.log(e);
