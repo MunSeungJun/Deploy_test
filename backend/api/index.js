07bee8accc61a26a6c7a2d77cb602562ab479816
MunSeungJun <sbs2469@naver.com>
2025-03-26 11:56:02 +0900

    cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,28 @@
 import { MongoClient } from "mongodb";
+import Cors from "cors";
 
-export default async function handler(req, res) { 
-    console.log(req)
-  const uri = "mongodb+srv://vercel-admin-user:8FPLjbXwx9znESJY@cluster0.jzdiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// CORS 미들웨어 생성
+const cors = Cors({
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  origin: "*",
+});
+
+// CORS 미들웨어 실행 함수
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors); // CORS 적용
+  const uri =
+    "mongodb+srv://vercel-admin-user:8FPLjbXwx9znESJY@cluster0.jzdiw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
   await client.connect();
   const db = client.db("hr");
@@ -21,16 +41,16 @@ export default async function handler(req, res) {
       });
     }
     if (req.method === "PUT") {
-        coll.updateOne({employee_id: req.params.id}, req.body)
-        res.status(200).json({
-            message: "success",
-          });
+      coll.updateOne({ employee_id: req.params.id }, req.body);
+      res.status(200).json({
+        message: "success",
+      });
     }
     if (req.method === "DELETE") {
-        coll.deleteOne({ employee_id: req.params.id })
-        res.status(200).json({
-            message: "success",
-          });
+      coll.deleteOne({ employee_id: req.params.id });
+      res.status(200).json({
+        message: "success",
+      });
     }
   } catch (e) {
     console.log(e);
